79bfdbc31aa12ebebe3b2ec3f0623ce9f5d97331
LuisCampelo2 <luisfilipecab@gmail.com>
2025-06-03 22:41:55 -0300

    arrumando cookies

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt';
 import { emailService } from '../services/email.service.js';
 import { userService } from '../services/user.service.js';
 import { jwtService } from '../services/jwt.service.js';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const register = async (req, res) => {
 
@@ -49,6 +50,8 @@ const login = async (req, res) => {
 
     res.cookie('accessToken', accessToken, {
       maxAge: 30 * 24 * 60 * 60 * 1000,
+      secure: isProduction, 
+      sameSite: isProduction ? 'none' : 'lax',
       httpOnly: true,
     })
     return res.json({ accessToken, user: normalizedUser, message: 'Login realizado com sucesso' });
@@ -62,6 +65,9 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
   try {
      res.clearCookie('accessToken', {
+      httpOnly: true,
+      secure: isProduction, 
+      sameSite: isProduction ? 'none' : 'lax',
       httpOnly: true,
     });
 
