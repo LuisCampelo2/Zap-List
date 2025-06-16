3e6a6a3591415eb98504aabe33857b564b31d562
LuisCampelo2 <luisfilipecab@gmail.com>
2025-06-16 14:31:07 -0300

    estilo do filter mudado

--- client/src/components/productsFilter.tsx
+++ client/src/components/productsFilter.tsx
@@ -27,48 +27,58 @@ export const ProductsFilter = () => {
   };
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <form role="search">
-            <div
-              style={{ border: "1px solid black", height:'30px' }}
-              className="container d-flex align-items-center gap-2 mt-3"
-            >
-              <i className="bi bi-search"></i>
-              <input
-                style={{width:'300px',border: 'none',background: 'none'}}
-                type="search"
-                onChange={handleInputChange}
-                value={searchParams.get("name") || ""}
-                placeholder="Pesquise o nome do produto..."
-                aria-label="Search"
-              />
-            </div>
-          </form>
-        </div>
-        <div className="row mt-3">
-          <select
-            className="form-select"
-            aria-label="Default select example"
-            onChange={handleSelectChange}
-            value={searchParams.get("category") || ""}
-          >
-            <option value="">Selecione categoria de Produtos</option>
-            <option value="Material de higiene">Material de higiene</option>
-            <option value="Material de limpeza">Material de limpeza</option>
-            <option value="Hortifruti">Hortifruti</option>
-            <option value="Carnes">Carnes</option>
-            <option value="Itens pra cachorro">Itens pra cachorro</option>
-            <option value="Alimentos não pereciveis">
-              Alimentos nao pereciveis
-            </option>
-            <option value="Bebidas">Bebidas</option>
-            <option value="Padaria">Padaria</option>
-            <option value="Temperos e especiarias">Temperos e especiarias</option>
-            <option value="Doces e guloseimas">Doces e guloseimas</option>
-            <option value="Laticinios e ovos">Laticinios e ovos</option>
-            <option value="Outros">Outros</option>
-          </select>
+      <div className="container container-filter mt-2">
+        <div className="card card-filter">
+          <div className="card-header card-header-filter">Buscar produto</div>
+          <div className="card-body">
+            <form role="search">
+              <div className="row">
+                <div
+                  className="container container-input"
+                >
+                  <i className="bi bi-search"></i>
+                  <input
+                    className="form-control input-search-products"
+                    type="search"
+                    onChange={handleInputChange}
+                    value={searchParams.get("name") || ""}
+                    placeholder="Pesquise o nome do produto..."
+                    aria-label="Search"
+                  />
+                </div>
+              </div>
+              <div className="row mt-3">
+                <select
+                  className="form-select select-search-products"
+                  aria-label="Default select example"
+                  onChange={handleSelectChange}
+                  value={searchParams.get("category") || ""}
+                >
+                  <option value="">Selecione categoria de Produtos</option>
+                  <option value="Material de higiene">
+                    Material de higiene
+                  </option>
+                  <option value="Material de limpeza">
+                    Material de limpeza
+                  </option>
+                  <option value="Hortifruti">Hortifruti</option>
+                  <option value="Carnes">Carnes</option>
+                  <option value="Itens pra cachorro">Itens pra cachorro</option>
+                  <option value="Alimentos não pereciveis">
+                    Alimentos nao pereciveis
+                  </option>
+                  <option value="Bebidas">Bebidas</option>
+                  <option value="Padaria">Padaria</option>
+                  <option value="Temperos e especiarias">
+                    Temperos e especiarias
+                  </option>
+                  <option value="Doces e guloseimas">Doces e guloseimas</option>
+                  <option value="Laticinios e ovos">Laticinios e ovos</option>
+                  <option value="Outros">Outros</option>
+                </select>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
