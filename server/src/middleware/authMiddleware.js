b0e11df32a33375c7e4f632d1e438501b956bbeb
LuisCampelo2 <luisfilipecab@gmail.com>
2025-08-02 20:08:34 -0300

    fazendo refresh token

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -2,18 +2,40 @@ import { jwtService } from "../services/jwt.service.js";
 
 const middleWare = (req, res, next) => {
   const token = req.cookies.accessToken;
+  const refreshToken = req.cookies.refreshToken;
 
-  if (!token) {
-    return res.status(401).json({ error: "Token não fornecido" });
+  if (!token && !refreshToken) {
+    return res.status(401).json({ error: "Nenhum token fornecido" });
   }
 
-  try {
-    const decoded = jwtService.verify(token);
+
+  const decoded = jwtService.verify(token);
+
+  if (decoded) {
     req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ error: "Token inválido" });
+    return next();
   }
+
+
+  const refreshDecoded = jwtService.verifyRefresh(refreshToken);
+
+  if (!refreshDecoded) {
+    return res.status(401).json({ error: "Tokens inválidos ou expirados" });
+  }
+
+
+  const newAccessToken = jwtService.sign(refreshDecoded);
+  res.cookie('accessToken', newAccessToken, {
+    maxAge: 15 * 60 * 1000, // 15 minutos
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    domain: process.env.FRONTEND_DOMAIN,
+  });
+
+  req.user = refreshDecoded;
+  return next();
 };
+
 
 export const authMiddleWare = { middleWare };
