20e7d46105920b11b5e1a5f64a1dd7f06d825619
LuisCampelo2 <luisfilipecab@gmail.com>
2025-08-07 00:34:16 -0300

    retirado function register do arquivo auth

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -5,19 +5,3 @@ export const logoutApi = async () => {
     withCredentials: true,
   });
 };
-
-export const registerApi = async (
-  email: string,
-  password: string,
-  name: string,
-  lastName: string
-) => {
-  const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/register`, {
-    email,
-    password,
-    name,
-    lastName,
-  });
-
-  return res;
-};
