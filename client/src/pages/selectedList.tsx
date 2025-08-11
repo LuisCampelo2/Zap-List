dcd8893dca5d30a47e6a4d1782b4c5fcbe788db8
LuisCampelo2 <luisfilipecab@gmail.com>
2025-08-11 14:00:34 -0300

    arrumado pagination em selectedList

--- client/src/pages/selectedList.tsx
+++ client/src/pages/selectedList.tsx
@@ -20,34 +20,33 @@ export const SelectedList = () => {
     (state: RootState) => state.listProduct.products
   );
   const page = useSelector((state: RootState) => state.listProduct.page);
-  const list=useSelector((state: RootState)=>state.listProduct.list)
+  const list = useSelector((state: RootState) => state.listProduct.list);
   const limitProducts = 10;
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(fetchProductsList({ id: Number(id) }));
   }, [page, id, dispatch]);
-  
-  const normalizedString = (str:string) => {
+
+  const normalizedString = (str: string) => {
     return str
       .normalize("NFD")
       .replace(/[\u0300-\u036f]/g, "")
       .replace(/\s+/g, "")
       .toLowerCase();
-  }
+  };
 
   const filteredProducts = products.filter((item) =>
     normalizedString(item.Product.name).includes(normalizedString(nameInput))
   );
 
-  const totalPages = Math.ceil(filteredProducts.length / limitProducts)
+  const totalPages = Math.ceil(filteredProducts.length / limitProducts);
 
   const paginatedProducts = filteredProducts.slice(
-  (page - 1) * limitProducts,
-  page * limitProducts
+    (page - 1) * limitProducts,
+    page * limitProducts
   );
-  
-  
+
   const handleModalOptions = (productItem: number) => {
     setOptions((prev) => (prev === productItem ? null : productItem));
   };
@@ -61,10 +60,10 @@ export const SelectedList = () => {
     setModalObservation((prev) => (prev === productId ? null : productId));
   };
 
-  const handleSearch = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setPage(1));
     setNameInput(e.target.value);
-  }
+  };
 
   return (
     <>
@@ -111,188 +110,176 @@ export const SelectedList = () => {
           </div>
         )}
         {products.length !== 0 && (
-           <div className="container">
-          <div className="card">
-            <div className="card-header">
-              <h1 className="col-12">Preço Estimado: R$ {list.totalPrice ?? 0}</h1>
-              <Link to={`/products?listId=${id}`} className="btn btn-all">
-                <i className="bi bi-cart-plus"></i>Adicionar Produto
-              </Link>
-              <div>
+          <div className="container">
+            <div className="card">
+              <div className="card-header">
+                <h1 className="col-12">
+                  Preço Estimado: R$ {list.totalPrice ?? 0}
+                </h1>
+                <Link to={`/products?listId=${id}`} className="btn btn-all">
+                  <i className="bi bi-cart-plus"></i>Adicionar Produto
+                </Link>
+                <div>
                   <input
-                  className="input-products-selected-list"
-                  id="nameInput"
-                  type="text"
-                  value={nameInput}
-                  onChange={(e)=>handleSearch(e)}
-                  placeholder="Buscar produto..."
-                />
+                    className="input-products-selected-list"
+                    id="nameInput"
+                    type="text"
+                    value={nameInput}
+                    onChange={(e) => handleSearch(e)}
+                    placeholder="Buscar produto..."
+                  />
+                </div>
               </div>
-            </div>
-            <div className="card-body">
-              <div className="table-responsive">
-                <table
-                  className="table table-container"
-                  style={{ width: "30%" }}
-                >
-                  <tbody>
-                    {paginatedProducts.map((productItem) => (
-                      <>
-                        <tr
-                          key={productItem.id}
-                          className={
-                            productItem.observation ? "observation" : ""
-                          }
+              <div className="card-body">
+                <div className="table-responsive">
+                  <table
+                    className="table table-container"
+                    style={{ width: "30%" }}
+                  >
+                    <tbody>
+                      {paginatedProducts.map((productItem) => (
+                        <>
+                          <tr
+                            key={productItem.id}
+                            className={
+                              productItem.observation ? "observation" : ""
+                            }
+                          >
+                            <td>
+                              <input
+                                className={
+                                  productItem.isChecked
+                                    ? "bg-success-subtle"
+                                    : ""
+                                }
+                                type="checkbox"
+                                id={`product-${productItem.id}`}
+                                checked={productItem.isChecked}
+                                onChange={() =>
+                                  dispatch(
+                                    checkBoxChange({
+                                      id: productItem.id,
+                                      isChecked: productItem.isChecked,
+                                    })
+                                  )
+                                }
+                              />
+                            </td>
+                            <td>
+                              <img
+                                style={{
+                                  width: "40px",
+                                  height: "40px",
+                                  objectFit: "cover",
+                                  objectPosition: "center",
+                                }}
+                                src={`${import.meta.env.VITE_API_URL}/imgs/${
+                                  productItem.Product.photo
+                                }`}
+                                alt=""
+                              />
+                            </td>
+                            <td>
+                              <label
+                                className="form-check-label"
+                                htmlFor={`product-${productItem.id}`}
+                              >
+                                {productItem.Product.name}
+                              </label>
+                            </td>
+                            <td>
+                              <strong>
+                                {productItem.quantity}
+                                {["Carnes", "Peixes"].includes(
+                                  productItem.Product.category
+                                )
+                                  ? " KG"
+                                  : " Unidade(s)"}
+                              </strong>
+                            </td>
+                            <td>
+                              <i
+                                onClick={() =>
+                                  setOptions((prev) =>
+                                    prev === productItem.id
+                                      ? null
+                                      : productItem.id
+                                  )
+                                }
+                                className="bi bi-three-dots-vertical"
+                              ></i>
+                              {options === productItem.id &&
+                                !modalObservation &&
+                                !modalDelete && (
+                                  <ModalOptions
+                                    productId={productItem.id}
+                                    product={productItem}
+                                    handleDelete={() =>
+                                      handleDelete(productItem.id)
+                                    }
+                                    handleObservation={() =>
+                                      openObservation(productItem.id)
+                                    }
+                                    onClose={() =>
+                                      handleModalOptions(productItem.id)
+                                    }
+                                  />
+                                )}
+                            </td>
+                          </tr>
+                        </>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              {totalPages > 1 && (
+                <div className="card-footer">
+                  <nav aria-label="...">
+                    <ul className="pagination d-flex justify-content-center">
+                      {page !== 1 && (
+                        <li>
+                          <a
+                            className="page-link"
+                            onClick={() => dispatch(setPage(page - 1))}
+                          >
+                            Voltar
+                          </a>
+                        </li>
+                      )}
+                      {[...Array(totalPages)].map((_, index) => (
+                        <li
+                          key={index}
+                          className={`page-item ${page === index + 1 ? "active" : ""}`}
                         >
-                          <td>
-                            <input
-                              className={
-                                productItem.isChecked ? "bg-success-subtle" : ""
-                              }
-                              type="checkbox"
-                              id={`product-${productItem.id}`}
-                              checked={productItem.isChecked}
-                              onChange={() =>
-                                dispatch(
-                                  checkBoxChange({
-                                    id: productItem.id,
-                                    isChecked: productItem.isChecked,
-                                  })
-                                )
-                              }
-                            />
-                          </td>
-                          <td>
-                            <img
-                              style={{
-                                width: "40px",
-                                height: "40px",
-                                objectFit: "cover",
-                                objectPosition: "center",
-                              }}
-                              src={`${import.meta.env.VITE_API_URL}/imgs/${
-                                productItem.Product.photo
-                              }`}
-                              alt=""
-                            />
-                          </td>
-                          <td>
-                            <label
-                              className="form-check-label"
-                              htmlFor={`product-${productItem.id}`}
-                            >
-                              {productItem.Product.name}
-                            </label>
-                          </td>
-                          <td>
-                            <strong>
-                              {productItem.quantity}
-                              {["Carnes", "Peixes"].includes(
-                                productItem.Product.category
-                              )
-                                ? " KG"
-                                : " Unidade(s)"}
-                            </strong>
-                          </td>
-                          <td>
-                            <i
-                              onClick={() =>
-                                setOptions((prev) =>
-                                  prev === productItem.id
-                                    ? null
-                                    : productItem.id
-                                )
-                              }
-                              className="bi bi-three-dots-vertical"
-                            ></i>
-                            {options === productItem.id &&
-                              !modalObservation &&
-                              !modalDelete && (
-                                <ModalOptions
-                                  productId={productItem.id}
-                                  product={productItem}
-                                  handleDelete={() =>
-                                    handleDelete(productItem.id)
-                                  }
-                                  handleObservation={() =>
-                                    openObservation(productItem.id)
-                                  }
-                                  onClose={() =>
-                                    handleModalOptions(productItem.id)
-                                  }
-                                />
-                              )}
-                          </td>
-                        </tr>
-                      </>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-            {totalPages > 1 && (
-              <div className="card-footer">
-                <nav aria-label="...">
-                  <ul className="pagination d-flex justify-content-center">
-                    {page !== 1 && (
+                          <a
+                            className="page-link"
+                            onClick={() => dispatch(setPage(index + 1))}
+                          >
+                            {index + 1}
+                          </a>
+                        </li>
+                      ))}
+                      {page !== totalPages && (
+                        <li className="page-item">
+                          <a
+                            className="page-link"
+                            onClick={() => dispatch(setPage(page + 1))}
+                          >
+                            Próxima
+                          </a>
+                        </li>
+                      )}
                       <li>
-                        <a
-                          className="page-link"
-                          onClick={() => dispatch(setPage(page - 1))}
-                        >
-                          Voltar
-                        </a>
+                        {" "}
                       </li>
-                    )}
-                    <li className={`page-item ${page === 1 ? "active" : ""}`}>
-                      <a
-                        className="page-link"
-                        onClick={() => dispatch(setPage(1))}
-                      >
-                        1
-                      </a>
-                    </li>
-                    <li className={`page-item ${page === 2 ? "active" : ""}`}>
-                      <a
-                        className="page-link"
-                        onClick={() => dispatch(setPage(2))}
-                        aria-current="page"
-                      >
-                        2
-                      </a>
-                    </li>
-                    <li className={`page-item ${page === 3 ? "active" : ""}`}>
-                      <a
-                        className="page-link"
-                        onClick={() => dispatch(setPage(3))}
-                      >
-                        3
-                      </a>
-                    </li>
-                    {page !== totalPages && (
-                      <li className="page-item">
-                        <a
-                          className="page-link"
-                          onClick={() => dispatch(setPage(page + 1))}
-                        >
-                          Próxima
-                        </a>
-                      </li>
-                    )}
-                    <li>
-                      {" "}
-                      <span>
-                        Página {page} de {totalPages}
-                      </span>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            )}
+                    </ul>
+                  </nav>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
         )}
-       
       </>
     </>
   );
